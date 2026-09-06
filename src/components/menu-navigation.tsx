1873c26e627723962337bd2b22150df63765c5b4
heri hermansyah <heryhermansyah563@gmail.com>
2026-09-06 23:43:01 +0700

    change(menu navigation): change menu navigation to LINK from a

--- src/components/menu-navigation.tsx
+++ src/components/menu-navigation.tsx
@@ -1,36 +1,30 @@
 import React from "react";
-import {cn} from "@/lib/utils";
+import { cn } from "@/lib/utils";
 import Link from "next/link";
 
 const menuData = [
-  {id: 1, name: "projects", link: "#projects"},
-  {id: 2, name: "skills", link: "#skills"},
-  {id: 3, name: "service", link: "#service"},
-  {id: 4, name: "contact", link: "#contact"},
+  { id: 1, name: "projects", link: "/portfolio" },
+  { id: 2, name: "service", link: "/service" },
+  { id: 3, name: "blog", link: "/blog" },
+  { id: 4, name: "about", link: "/about" },
 ];
 
 interface MenuNavigationProps {
   className?: React.ReactNode;
 }
 
-const MenuNavigation = ({className}: MenuNavigationProps) => {
+const MenuNavigation = ({ className }: MenuNavigationProps) => {
   return (
     <nav aria-label="menu header">
       <ul className={cn("flex items-center gap-5 capitalize", className)}>
-        <Link
-          href="/"
-          className="text-Dark-Maroon font-semibold text-[18px] lg:text-[20px]"
-        >
-          home
-        </Link>
         {menuData.map((item) => (
           <li key={item.id}>
-            <a
+            <Link
               href={item.link}
               className="text-Dark-Maroon font-semibold text-[18px] lg:text-[20px]"
             >
               {item.name}
-            </a>
+            </Link>
           </li>
         ))}
       </ul>
