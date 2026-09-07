a4b913bebbcba3af0599fb7b74dd49f53bf94f34
heri hermansyah <heryhermansyah563@gmail.com>
2026-09-08 01:33:18 +0700

    feat(logo): add childmotion

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,17 +1,19 @@
 import Link from "next/link";
-import React from "react";
+import { ChildMotion } from "./ui/wrapper-motion";
 
 const Logo = () => {
   return (
-    <Link href={"/"} className="flex items-center gap-1 uppercase">
-      <h1 className="text-Dark-Maroon text-[24px] sm:text-[36px] font-bold">
-        Hermansyah
-      </h1>
-      <div className="w-0.5 h-7 sm:h-8 bg-Dark-Maroon" />
-      <h2 className="flex flex-col leading-4 text-Dark-Maroon/80 font-semibold">
-        <span>Frontend</span> <span>Developer</span>
-      </h2>
-    </Link>
+    <ChildMotion>
+      <Link href={"/"} className="flex items-center gap-1 uppercase">
+        <h1 className="text-Dark-Maroon text-[24px] sm:text-[36px] font-bold">
+          Hermansyah
+        </h1>
+        <div className="w-0.5 h-7 sm:h-8 bg-Dark-Maroon" />
+        <h2 className="flex flex-col leading-4 text-Dark-Maroon/80 font-semibold">
+          <span>Frontend</span> <span>Developer</span>
+        </h2>
+      </Link>
+    </ChildMotion>
   );
 };
 
